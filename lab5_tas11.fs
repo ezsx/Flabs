94ea8357cb086a8c343e6a1770eff0c94ffb61d9
ezsx <scdcor@gmail.com>
2022-03-19 04:00:27 +0300

    о боже бил гейтс, хорошо прячешь гит... но все таки ты был пойман на ошибке

---
+++ lab5_tas11.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let answer lang = 
+    match lang with
+        |"F#" | "Prolog" -> printfn "Подлиза"
+        |"python" -> printfn "ясно лох"
+        |"С++" -> printfn "а че у дурова конкурс не выйграл на маски в тг?"
+        |"ruby" -> printfn "ты ваще в курсах что это?"
+        |"css" -> printfn "ты че даун?"
+        |"С#" -> printfn "ясно дефчушка"
+        |"php" -> printfn "а че не css?"
+        |other -> printfn "%s"(lang + "чел мне лень писать ответ под твой ЯП, но если это abap то я тебе сочувствую")
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введи любимый язык программирования:"
+    let input = Console.ReadLine()
+    
+    answer input
+    0
