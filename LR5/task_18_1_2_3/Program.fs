747e138f22ca96b748dd19cf8111e440ebcf52be
ezsx <scdcor@gmail.com>
2022-03-27 22:13:06 +0300

    решил 18_3

--- LR5/task_18_1_2_3/Program.fs
+++ LR5/task_18_1_2_3/Program.fs
@@ -25,10 +25,30 @@ let digitBypassWithPredicate number predicate =
     let rec procInternal x mult =
         if(x = 0) then mult
         else
-            let newList = if predicate (x % 10) then mult*(x%10) else mult
+            let newM = if predicate (x % 10) then mult*(x%10) else mult
             let newX = x / 10
-            procInternal newX newList
+            procInternal newX newM
     procInternal number 1
+
+let rec nod x y =
+    if x = 0 || y = 0 then x + y      
+    else
+        let newX = if x > y then x % y else x
+        let newY = if x <= y then y % x else y
+        nod newX newY
+
+let dividersFunc x func init =
+    let rec df x func init curDiv =
+        if curDiv = 0 then init
+        else
+            let newInit= if x % curDiv = 0 then func init curDiv else init
+            let newDiv = curDiv - 1
+            df x func newInit newDiv
+    df x func init x
+
+let dividersFuncWithPredicate x predicate func init =
+    let func1 init cur = if predicate cur then func init cur else init
+    dividersFunc x func1 init
 
 // LR5_17.1- Обход делителей числа с условием
 let div_F_pred x predicate func init =
@@ -40,6 +60,10 @@ let summ_del x =
 
 let mult_num x =
     digitBypassWithPredicate x (fun x -> x%5<>0)
+
+let nod_mega x=
+    nod (dividersFuncWithPredicate x (fun x -> x%2 > 0 && not(isPrime x)) (fun x y -> max x y) 1) (digitBypassWithPredicate x (fun x -> true))
+    
     
 
 [<EntryPoint>]
@@ -52,6 +76,10 @@ let main argv =
     printfn "максимальный простой делитель числа: %d" t181
 
     let t182 = mult_num x
-    printfn "Найти произведение цифр числа, не делящихся на 5: %d" t182
+    printfn "произведение цифр числа, не делящихся на 5: %d" t182
+
+    let t183 = nod_mega x
+    printfn "сумма всех делителей числа, взаимно простых с суммой
+    цифр числа и не взаимно простых с произведением цифр числа: %d" t183
 
     0
