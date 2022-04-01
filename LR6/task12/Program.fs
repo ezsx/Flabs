a97ad82941772794b61a66666bb32b9ccf2c9e31
ezsx <scdcor@gmail.com>
2022-04-01 23:01:36 +0300

    задание 12
    задание 12

---
+++ LR6/task12/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+let elemCount list el =
+    let rec elemCountInternal list el count  =
+        match list with
+        | [] -> count
+        | el0::t ->
+            let newCount = if (el0 = el) then count + 1 else count
+            elemCountInternal t el newCount
+    elemCountInternal list el 0
+
+let proc list =
+    let rec procInternal list or_List el =
+        match list with
+        | [] -> el
+        | el0::t ->
+            let newEl = if (elemCount or_List el0) = 1 then el0 else el
+            procInternal t or_List newEl
+    procInternal list list list.Head
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Количество элементов и список:"
+
+    let list = Program.read_L (Console.ReadLine() |> Convert.ToInt32)
+   
+    printfn "Результат: %d" (proc list)
+
+    0
