07062847c350dc1fd571dd0ac641eaff0b904db9
ezsx <scdcor@gmail.com>
2022-04-02 04:59:19 +0300

    39

---
+++ LR6/task_39/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let div L t predicate=
+    let rec out L t=
+        match L with
+        |[]-> 0
+        |(i:int)::s->
+           if predicate t then 
+             System.Console.WriteLine(i) 
+             let newN = t - 1
+             let newL = s
+             out newL newN
+           else 
+             let newN = t - 1
+             let newL=s
+             out newL newN
+    out L t     
+ 
+[<EntryPoint>]
+let main argv =
+    printfn "Количество элементов и список:"
+    let i = Console.ReadLine() |> Convert.ToInt32
+    let l = Program.readL(i)
+    div l i (fun x -> if x%2=0 then true else false)|>ignore
+    div l i (fun x -> if x%2=1 then true else false)|>ignore
+    0 
