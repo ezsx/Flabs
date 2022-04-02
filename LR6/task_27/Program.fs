3e5acffca83f74e518b2fa329bdfe1b1dc812d13
ezsx <scdcor@gmail.com>
2022-04-02 03:47:15 +0300

    задание 27, решено

---
+++ LR6/task_27/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let Shift L =
+    let rec out L i cc =
+        if cc <> 0 then L @ [i] else
+        match L with
+        |[] -> L
+        |i::t ->
+            let newcc = cc + 1
+            out t i newcc 
+    out L 0 0
+[<EntryPoint>]
+let main argv =
+    printfn "Количество элементов и список:"
+    Program.printL (Shift (Program.readL(Console.ReadLine() |> Convert.ToInt32))) 
+    0 
